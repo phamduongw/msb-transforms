e6a5564f6daf3c5286edb13fba7947b6ebc109d9
Duong Pham <duongpq@bnh.vn>
2025-09-12 17:14:10 +0700

    Update DDL

--- jobs/T24_LNMEMO_ACTIVITY_JOB.sql
+++ jobs/T24_LNMEMO_ACTIVITY_JOB.sql
@@ -1,15 +1,15 @@
--- DROP JOB
-BEGIN DBMS_SCHEDULER.drop_job('T24_LNMEMO_ACTIVITY_GEN_FROM_ACC_JOB', TRUE); END;
-BEGIN DBMS_SCHEDULER.drop_job('T24_LNMEMO_ACTIVITY_GEN_FROM_ARR_JOB', TRUE); END;
-BEGIN DBMS_SCHEDULER.drop_job('T24_LNMEMO_ACTIVITY_GEN_FROM_BIL_JOB', TRUE); END;
-BEGIN DBMS_SCHEDULER.drop_job('T24_LNMEMO_ACTIVITY_GEN_FROM_ECB_JOB', TRUE); END;
+-- DROP
+BEGIN DBMS_SCHEDULER.drop_job('T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_ACC_JOB', TRUE); END;
+BEGIN DBMS_SCHEDULER.drop_job('T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_ARR_JOB', TRUE); END;
+BEGIN DBMS_SCHEDULER.drop_job('T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_BIL_JOB', TRUE); END;
+BEGIN DBMS_SCHEDULER.drop_job('T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_ECB_JOB', TRUE); END;
 
--- T24_LNMEMO_ACTIVITY_GEN_FROM_ACC_JOB
+-- ACC
 BEGIN
     DBMS_SCHEDULER.create_job(
-            job_name => 'T24_LNMEMO_ACTIVITY_GEN_FROM_ACC_JOB',
-            job_type => 'PLSQL_BLOCK',
-            job_action => q'[
+        job_name   => 'T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_ACC_JOB',
+        job_type   => 'PLSQL_BLOCK',
+        job_action => q'[
             BEGIN
                 EXECUTE IMMEDIATE 'ALTER SESSION SET NLS_NUMERIC_CHARACTERS = ''.,''';
                 LOOP
@@ -17,18 +17,18 @@ BEGIN
                 END LOOP;
             END;
         ]',
-            start_date => SYSTIMESTAMP,
-            enabled => FALSE,
-            auto_drop => FALSE
+        start_date => SYSTIMESTAMP,
+        enabled    => FALSE,
+        auto_drop  => FALSE
     );
 END;
 
--- T24_LNMEMO_ACTIVITY_GEN_FROM_ARR_JOB
+-- ARR
 BEGIN
     DBMS_SCHEDULER.create_job(
-            job_name => 'T24_LNMEMO_ACTIVITY_GEN_FROM_ARR_JOB',
-            job_type => 'PLSQL_BLOCK',
-            job_action => q'[
+        job_name   => 'T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_ARR_JOB',
+        job_type   => 'PLSQL_BLOCK',
+        job_action => q'[
             BEGIN
                 EXECUTE IMMEDIATE 'ALTER SESSION SET NLS_NUMERIC_CHARACTERS = ''.,''';
                 LOOP
@@ -36,18 +36,18 @@ BEGIN
                 END LOOP;
             END;
         ]',
-            start_date => SYSTIMESTAMP,
-            enabled => FALSE,
-            auto_drop => FALSE
+        start_date => SYSTIMESTAMP,
+        enabled    => FALSE,
+        auto_drop  => FALSE
     );
 END;
 
--- T24_LNMEMO_ACTIVITY_GEN_FROM_BIL_JOB
+-- BIL
 BEGIN
     DBMS_SCHEDULER.create_job(
-            job_name => 'T24_LNMEMO_ACTIVITY_GEN_FROM_BIL_JOB',
-            job_type => 'PLSQL_BLOCK',
-            job_action => q'[
+        job_name   => 'T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_BIL_JOB',
+        job_type   => 'PLSQL_BLOCK',
+        job_action => q'[
             BEGIN
                 EXECUTE IMMEDIATE 'ALTER SESSION SET NLS_NUMERIC_CHARACTERS = ''.,''';
                 LOOP
@@ -55,18 +55,18 @@ BEGIN
                 END LOOP;
             END;
         ]',
-            start_date => SYSTIMESTAMP,
-            enabled => FALSE,
-            auto_drop => FALSE
+        start_date => SYSTIMESTAMP,
+        enabled    => FALSE,
+        auto_drop  => FALSE
     );
 END;
 
--- T24_LNMEMO_ACTIVITY_GEN_FROM_ECB_JOB
+-- ECB
 BEGIN
     DBMS_SCHEDULER.create_job(
-            job_name => 'T24_LNMEMO_ACTIVITY_GEN_FROM_ECB_JOB',
-            job_type => 'PLSQL_BLOCK',
-            job_action => q'[
+        job_name   => 'T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_ECB_JOB',
+        job_type   => 'PLSQL_BLOCK',
+        job_action => q'[
             BEGIN
                 EXECUTE IMMEDIATE 'ALTER SESSION SET NLS_NUMERIC_CHARACTERS = ''.,''';
                 LOOP
@@ -74,33 +74,33 @@ BEGIN
                 END LOOP;
             END;
         ]',
-            start_date => SYSTIMESTAMP,
-            enabled => FALSE,
-            auto_drop => FALSE
+        start_date => SYSTIMESTAMP,
+        enabled    => FALSE,
+        auto_drop  => FALSE
     );
 END;
 
--- START JOB
-BEGIN DBMS_SCHEDULER.run_job('T24_LNMEMO_ACTIVITY_GEN_FROM_ACC_JOB', FALSE); END;
-BEGIN DBMS_SCHEDULER.run_job('T24_LNMEMO_ACTIVITY_GEN_FROM_ARR_JOB', FALSE); END;
-BEGIN DBMS_SCHEDULER.run_job('T24_LNMEMO_ACTIVITY_GEN_FROM_BIL_JOB', FALSE); END;
-BEGIN DBMS_SCHEDULER.run_job('T24_LNMEMO_ACTIVITY_GEN_FROM_ECB_JOB', FALSE); END;
+-- STOP
+BEGIN DBMS_SCHEDULER.stop_job('T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_ACC_JOB', TRUE); END;
+BEGIN DBMS_SCHEDULER.stop_job('T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_ARR_JOB', TRUE); END;
+BEGIN DBMS_SCHEDULER.stop_job('T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_BIL_JOB', TRUE); END;
+BEGIN DBMS_SCHEDULER.stop_job('T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_ECB_JOB', TRUE); END;
 
--- STOP JOB
-BEGIN DBMS_SCHEDULER.stop_job('T24_LNMEMO_ACTIVITY_GEN_FROM_ACC_JOB', TRUE); END;
-BEGIN DBMS_SCHEDULER.stop_job('T24_LNMEMO_ACTIVITY_GEN_FROM_ARR_JOB', TRUE); END;
-BEGIN DBMS_SCHEDULER.stop_job('T24_LNMEMO_ACTIVITY_GEN_FROM_BIL_JOB', TRUE); END;
-BEGIN DBMS_SCHEDULER.stop_job('T24_LNMEMO_ACTIVITY_GEN_FROM_ECB_JOB', TRUE); END;
+-- RUN
+BEGIN DBMS_SCHEDULER.run_job('T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_ACC_JOB', FALSE); END;
+BEGIN DBMS_SCHEDULER.run_job('T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_ARR_JOB', FALSE); END;
+BEGIN DBMS_SCHEDULER.run_job('T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_BIL_JOB', FALSE); END;
+BEGIN DBMS_SCHEDULER.run_job('T24RAW.T24_LNMEMO_ACTIVITY_GEN_FROM_ECB_JOB', FALSE); END;
 
--- STATUS JOB
+-- STATUS
 SELECT JOB_NAME, STATE, LAST_START_DATE, NEXT_RUN_DATE, RUN_COUNT, FAILURE_COUNT
 FROM DBA_SCHEDULER_JOBS
-WHERE JOB_NAME LIKE 'T24_LNMEMO_ACTIVITY%';
+WHERE OWNER = 'T24RAW';
 
--- ERROR JOB
+-- ERROR
 SELECT JOB_NAME, STATUS, ACTUAL_START_DATE, RUN_DURATION, ERROR#, ADDITIONAL_INFO
 FROM DBA_SCHEDULER_JOB_RUN_DETAILS
-WHERE STATUS = 'FAILED'
-AND ACTUAL_START_DATE > TO_TIMESTAMP('2025-07-14 00:00:00.000', 'YYYY-MM-DD HH24:MI:SS.FF3')
-AND JOB_NAME LIKE 'T24_LNMEMO_ACTIVITY%';
+WHERE OWNER = 'T24RAW'
+  AND STATUS = 'FAILED'
+  AND ACTUAL_START_DATE > TO_TIMESTAMP('2025-09-10 00:00:00.000', 'YYYY-MM-DD HH24:MI:SS.FF3')
 ORDER BY ACTUAL_START_DATE DESC;
