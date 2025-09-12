a239556fcc96e740cdae32fe250e02dac7a491c2
Duong Pham <duongpq@bnh.vn>
2025-09-13 00:43:52 +0700

    Update

--- all.sh
+++ all.sh
@@ -3,6 +3,7 @@
 > _ALL.sql
 
 find tables -type f -name "*.sql" -exec sh -c 'cat "$1" >> _ALL.sql; echo "" >> _ALL.sql' _ {} \;
+find views -type f -name "*.sql" -exec sh -c 'cat "$1" >> _ALL.sql; echo "" >> _ALL.sql' _ {} \;
 find types -type f -name "*.sql" -exec sh -c 'cat "$1" >> _ALL.sql; echo "" >> _ALL.sql' _ {} \;
 cat packages/T24_UTILS_PKG.sql >> _ALL.sql
 echo "" >> _ALL.sql
