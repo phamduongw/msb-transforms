ff6ada8be024729bcbb74e363732ac9685269bfd
Duong Pham <duongpq@bnh.vn>
2025-10-07 20:55:27 +0700

    Update gather script

--- scripts/GATHER.sql
+++ scripts/GATHER.sql
@@ -4,8 +4,9 @@ BEGIN
    DBMS_STATS.GATHER_SCHEMA_STATS(
       ownname          => 'T24RAWOGG',
       estimate_percent => DBMS_STATS.AUTO_SAMPLE_SIZE,
-      method_opt       => 'FOR ALL COLUMNS SIZE AUTO',
+      method_opt       => 'FOR ALL COLUMNS SIZE SKEWONLY',
+      degree           => 16,
       cascade          => TRUE,
-      degree           => DBMS_STATS.DEFAULT_DEGREE
+      no_invalidate    => FALSE
    );
 END;
