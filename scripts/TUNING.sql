685deb67a739f39f547b88184eda2f6b47a95698
Duong Pham <duongpq@bnh.vn>
2025-10-17 13:23:38 +0700

    UPDATE

---
+++ scripts/TUNING.sql
@@ -0,0 +1,30 @@
+PURGE RECYCLEBIN;
+
+ALTER INDEX T24RAWOGG.IDX_FMSB_BIL_LNMEMO_ARRANGEMENT_ID REBUILD ONLINE NOLOGGING;
+
+BEGIN DBMS_STATS.GATHER_TABLE_STATS(
+      ownname          => 'T24RAWOGG',
+      tabname          => 'FMSB_ACC_MAPPED',
+      estimate_percent => DBMS_STATS.AUTO_SAMPLE_SIZE,
+      method_opt       => 'FOR ALL COLUMNS SIZE SKEWONLY',
+      degree           => 64,
+      cascade          => TRUE,
+      no_invalidate    => FALSE
+   );
+END;
+
+BEGIN DBMS_STATS.GATHER_SCHEMA_STATS(
+      ownname          => 'T24RAWOGG',
+      estimate_percent => DBMS_STATS.AUTO_SAMPLE_SIZE,
+      method_opt       => 'FOR ALL COLUMNS SIZE SKEWONLY',
+      degree           => 16,
+      cascade          => TRUE,
+      no_invalidate    => FALSE
+   );
+END;
+
+SELECT LIMIT_KEY, COUNT(*) AS CNT
+FROM V_FMSB_ACC_MAPPED
+GROUP BY LIMIT_KEY
+HAVING COUNT(*) > 1
+ORDER BY CNT DESC;
