e719af196cc705e3282960fd212bfe7156982aa7
Duong Pham <duongpq@bnh.vn>
2025-11-10 18:15:58 +0700

    Update code LMT for LNMEMO

--- jobs/T24_LNMEMO_ACTIVITY_JOB.sql
+++ jobs/T24_LNMEMO_ACTIVITY_JOB.sql
@@ -1,13 +1,13 @@
--- ACC_ARR_ECB
+-- ACC_ARR_ECB_LMT
 BEGIN
     DBMS_SCHEDULER.create_job(
-        job_name   => 'T24RAWOGG.T24_LNMEMO_ACTIVITY_GEN_FROM_ACC_ARR_ECB_JOB',
+        job_name   => 'T24RAWOGG.T24_LNMEMO_ACTIVITY_GEN_FROM_ACC_ARR_ECB_LMT_JOB',
         job_type   => 'PLSQL_BLOCK',
         job_action => q'[
             BEGIN
                 EXECUTE IMMEDIATE 'ALTER SESSION SET NLS_NUMERIC_CHARACTERS = ''.,''';
                 LOOP
-                    T24RAWOGG.T24_LNMEMO_ACTIVITY_PKG.GEN_FROM_ACC_ARR_ECB_PROC;
+                    T24RAWOGG.T24_LNMEMO_ACTIVITY_PKG.GEN_FROM_ACC_ARR_ECB_LMT_PROC;
                 END LOOP;
             END;
         ]',
@@ -17,7 +17,7 @@ BEGIN
     );
 
     DBMS_SCHEDULER.set_attribute(
-        name      => 'T24RAWOGG.T24_LNMEMO_ACTIVITY_GEN_FROM_ACC_ARR_ECB_JOB',
+        name      => 'T24RAWOGG.T24_LNMEMO_ACTIVITY_GEN_FROM_ACC_ARR_ECB_LMT_JOB',
         attribute => 'instance_id',
         value     => 1
     );
@@ -43,31 +43,6 @@ BEGIN
 
     DBMS_SCHEDULER.set_attribute(
         name      => 'T24RAWOGG.T24_LNMEMO_ACTIVITY_GEN_FROM_BIL_JOB',
-        attribute => 'instance_id',
-        value     => 1
-    );
-END;
-
--- LMT
-BEGIN
-    DBMS_SCHEDULER.create_job(
-        job_name   => 'T24RAWOGG.T24_LNMEMO_ACTIVITY_GEN_FROM_LMT_JOB',
-        job_type   => 'PLSQL_BLOCK',
-        job_action => q'[
-            BEGIN
-                EXECUTE IMMEDIATE 'ALTER SESSION SET NLS_NUMERIC_CHARACTERS = ''.,''';
-                LOOP
-                    T24RAWOGG.T24_LNMEMO_ACTIVITY_PKG.GEN_FROM_LMT_PROC;
-                END LOOP;
-            END;
-        ]',
-        start_date => SYSTIMESTAMP,
-        enabled    => FALSE,
-        auto_drop  => FALSE
-    );
-
-    DBMS_SCHEDULER.set_attribute(
-        name      => 'T24RAWOGG.T24_LNMEMO_ACTIVITY_GEN_FROM_LMT_JOB',
         attribute => 'instance_id',
         value     => 1
     );
--- packages/T24_LNMEMO_ACTIVITY_PKG.sql
+++ packages/T24_LNMEMO_ACTIVITY_PKG.sql
@@ -18,9 +18,8 @@ CREATE OR REPLACE PACKAGE T24RAWOGG.T24_LNMEMO_ACTIVITY_PKG IS
         P_DEBIT_MVMT      IN VARCHAR2
     ) RETURN NUMBER;
 
-    PROCEDURE GEN_FROM_ACC_ARR_ECB_PROC;
+    PROCEDURE GEN_FROM_ACC_ARR_ECB_LMT_PROC;
     PROCEDURE GEN_FROM_BIL_PROC;
-    PROCEDURE GEN_FROM_LMT_PROC;
 
 END T24_LNMEMO_ACTIVITY_PKG;
 
@@ -153,15 +152,15 @@ CREATE OR REPLACE PACKAGE BODY T24RAWOGG.T24_LNMEMO_ACTIVITY_PKG IS
     END CALC_ACCINT_VAL_FUNC;
 
     ---------------------------------------------------------------------------
-    -- GEN_FROM_ACC_ARR_ECB_PROC
+    -- GEN_FROM_ACC_ARR_ECB_LMT_PROC
     ---------------------------------------------------------------------------
-    PROCEDURE GEN_FROM_ACC_ARR_ECB_PROC IS
+    PROCEDURE GEN_FROM_ACC_ARR_ECB_LMT_PROC IS
         V_JOIN_KEY_LIST  T_JOIN_KEY_ARRAY;
         V_WINDOW_ID_LIST T_WINDOW_ID_ARRAY;
     BEGIN
         SELECT CDC.JOIN_KEY, CDC.WINDOW_ID
         BULK COLLECT INTO V_JOIN_KEY_LIST, V_WINDOW_ID_LIST
-        FROM T24_LNMEMO_ACTIVITY_ACC_ARR_ECB CDC
+        FROM T24_LNMEMO_ACTIVITY_ACC_ARR_ECB_LMT CDC
         WHERE EXISTS (
             SELECT 1
             FROM V_FMSB_ECB_MAPPED ECB
@@ -176,12 +175,17 @@ CREATE OR REPLACE PACKAGE BODY T24RAWOGG.T24_LNMEMO_ACTIVITY_PKG IS
             SELECT 1
             FROM V_FMSB_ARR_LNMEMO ARR
             WHERE ARR.WINDOW_ID = CDC.WINDOW_ID
+        )
+        OR EXISTS (
+            SELECT 1
+            FROM V_FMSB_LMT_MAPPED LMT
+            WHERE LMT.WINDOW_ID = CDC.WINDOW_ID
         );
         -- ) FETCH FIRST 9999 ROWS ONLY;
 
         IF V_JOIN_KEY_LIST.COUNT > 0 THEN
 
-            DELETE FROM T24_LNMEMO_ACTIVITY_ACC_ARR_ECB CDC
+            DELETE FROM T24_LNMEMO_ACTIVITY_ACC_ARR_ECB_LMT CDC
             WHERE EXISTS (
                 SELECT 1
                 FROM TABLE(V_WINDOW_ID_LIST) TMP
@@ -267,7 +271,7 @@ CREATE OR REPLACE PACKAGE BODY T24RAWOGG.T24_LNMEMO_ACTIVITY_PKG IS
                 PRE.COMMIT_TS, -- COMMIT_TS
                 PRE.REPLICAT_TS, -- REPLICAT_TS
                 PRE.MAPPED_TS, -- MAPPED_TS
-                'ACC_ARR_ECB' -- CALL_CDC
+                'ACC_ARR_ECB_LMT' -- CALL_CDC
             FROM PRECOMPUTED PRE
             LEFT JOIN AGGREGATED AGG ON AGG.ARRANGEMENT_ID = PRE.ARR_RECID;
 
@@ -277,7 +281,7 @@ CREATE OR REPLACE PACKAGE BODY T24RAWOGG.T24_LNMEMO_ACTIVITY_PKG IS
         WHEN OTHERS THEN
             ROLLBACK;
             RAISE;
-    END GEN_FROM_ACC_ARR_ECB_PROC;
+    END GEN_FROM_ACC_ARR_ECB_LMT_PROC;
 
     ---------------------------------------------------------------------------
     -- GEN_FROM_BIL_PROC
@@ -377,117 +381,5 @@ CREATE OR REPLACE PACKAGE BODY T24RAWOGG.T24_LNMEMO_ACTIVITY_PKG IS
             ROLLBACK;
             RAISE;
     END GEN_FROM_BIL_PROC;
-
-    ---------------------------------------------------------------------------
-    -- GEN_FROM_LMT_PROC
-    ---------------------------------------------------------------------------
-    PROCEDURE GEN_FROM_LMT_PROC IS
-        V_WINDOW_ID_LIST T_WINDOW_ID_ARRAY;
-    BEGIN
-        SELECT CDC.WINDOW_ID
-        BULK COLLECT INTO V_WINDOW_ID_LIST
-        FROM T24_LNMEMO_ACTIVITY_LMT CDC
-        WHERE EXISTS (
-            SELECT 1
-            FROM V_FMSB_LMT_MAPPED LMT
-            WHERE LMT.WINDOW_ID = CDC.WINDOW_ID
-        );
-        -- ) FETCH FIRST 9999 ROWS ONLY;
-
-        IF V_WINDOW_ID_LIST.COUNT > 0 THEN
-
-            DELETE FROM T24_LNMEMO_ACTIVITY_LMT CDC
-            WHERE EXISTS (
-                SELECT 1
-                FROM TABLE(V_WINDOW_ID_LIST) TMP
-                WHERE TMP.COLUMN_VALUE = CDC.WINDOW_ID
-            );
-
-            INSERT INTO T24_LNMEMO_ACTIVITY (
-                ACCTNO,
-                CURTYP,
-                CBAL,
-                HOLD,
-                DRLIMT,
-                ACCINT,
-                COMACC,
-                OTHCHG,
-                BILPRN,
-                BILINT,
-                BILESC,
-                BILLC,
-                BILOC,
-                BILMC,
-                WINDOW_ID,
-                COMMIT_TS,
-                REPLICAT_TS,
-                MAPPED_TS,
-                CALL_CDC
-            )
-            WITH PRECOMPUTED AS (
-                SELECT /*+ MATERIALIZE */
-                    ARR.RECID           AS ARR_RECID,
-                    ARR.LINKED_APPL_ID  AS ACCTNO,
-                    ACC.CURRENCY        AS CURTYP,
-                    ECB.CURR_ASSET_TYPE AS CURR_ASSET_TYPE,
-                    ECB.OPEN_BALANCE    AS OPEN_BALANCE,
-                    ECB.CREDIT_MVMT     AS CREDIT_MVMT,
-                    ECB.DEBIT_MVMT      AS DEBIT_MVMT,
-                    ACC.LOCKED_AMOUNT   AS LOCKED_AMOUNT,
-                    LMT.INTERNAL_AMOUNT AS DRLIMT,
-                    LMT.WINDOW_ID       AS WINDOW_ID,
-                    LMT.COMMIT_TS       AS COMMIT_TS,
-                    LMT.REPLICAT_TS     AS REPLICAT_TS,
-                    LMT.MAPPED_TS       AS MAPPED_TS
-                FROM TABLE(V_WINDOW_ID_LIST) TMP
-                INNER JOIN V_FMSB_LMT_MAPPED LMT ON LMT.WINDOW_ID      = TMP.COLUMN_VALUE
-                INNER JOIN V_FMSB_ACC_MAPPED ACC ON ACC.LIMIT_KEY      = LMT.RECID
-                INNER JOIN V_FMSB_ARR_LNMEMO ARR ON ARR.LINKED_APPL_ID = ACC.RECID
-                INNER JOIN V_FMSB_ECB_MAPPED ECB ON ECB.RECID          = ACC.RECID
-            ),
-            AGGREGATED AS (
-                SELECT
-                    BIL.ARRANGEMENT_ID,
-                    SUM(BILPRN_AMT) AS BILPRN,
-                    SUM(BILINT_AMT) AS BILINT,
-                    SUM(BILLC_AMT)  AS BILLC
-                FROM V_FMSB_BIL_LNMEMO BIL
-                WHERE EXISTS (
-                    SELECT 1
-                    FROM PRECOMPUTED PRE
-                    WHERE PRE.ARR_RECID = BIL.ARRANGEMENT_ID
-                )
-                GROUP BY BIL.ARRANGEMENT_ID
-            )
-            SELECT 
-                TO_NUMBER(PRE.ACCTNO), -- ACCTNO
-                PRE.CURTYP, -- CURTYP
-                CALC_CBAL_VAL_FUNC(PRE.CURR_ASSET_TYPE, PRE.OPEN_BALANCE, PRE.CREDIT_MVMT, PRE.DEBIT_MVMT), -- CBAL
-                CALC_HOLD_VAL_FUNC(PRE.LOCKED_AMOUNT), -- HOLD
-                NVL(TO_NUMBER(PRE.DRLIMT), 0), -- DRLIMT
-                CALC_ACCINT_VAL_FUNC(PRE.CURR_ASSET_TYPE, PRE.OPEN_BALANCE, PRE.CREDIT_MVMT, PRE.DEBIT_MVMT), -- ACCINT
-                0, -- COMACC
-                0, -- OTHCHG
-                NVL(AGG.BILPRN, 0), -- BILPRN
-                NVL(AGG.BILINT, 0), -- BILINT
-                0, -- BILESC
-                NVL(AGG.BILLC, 0), -- BILLC
-                0, -- BILOC
-                0, -- BILMC
-                PRE.WINDOW_ID, -- WINDOW_ID
-                PRE.COMMIT_TS, -- COMMIT_TS
-                PRE.REPLICAT_TS, -- REPLICAT_TS
-                PRE.MAPPED_TS, -- MAPPED_TS
-                'LMT' -- CALL_CDC
-            FROM PRECOMPUTED PRE
-            LEFT JOIN AGGREGATED AGG ON AGG.ARRANGEMENT_ID = PRE.ARR_RECID;
-
-            COMMIT;
-        END IF;
-    EXCEPTION
-        WHEN OTHERS THEN
-            ROLLBACK;
-            RAISE;
-    END GEN_FROM_LMT_PROC;
 
 END T24_LNMEMO_ACTIVITY_PKG;
