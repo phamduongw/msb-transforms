0bfd8d032f5771bbeaab8e7447e0fb8a449b7e23
duogwas <duong2001n@gmail.com>
2025-11-11 14:20:40 +0700

    update lmt job

--- packages/T24_DDMEMO_ACTIVITY_PKG.sql
+++ packages/T24_DDMEMO_ACTIVITY_PKG.sql
@@ -18,7 +18,7 @@ CREATE OR REPLACE PACKAGE T24RAWOGG.T24_DDMEMO_ACTIVITY_PKG IS
         P_DEBIT_MVMT      IN VARCHAR2
     ) RETURN NUMBER;
 
-    PROCEDURE GEN_FROM_ACC_ECB_ARR_PROC;
+    PROCEDURE GEN_FROM_ACC_ARR_ECB_LMT_PROC;
 
     PROCEDURE GEN_FROM_LMT_PROC;
 
@@ -160,16 +160,16 @@ CREATE OR REPLACE PACKAGE BODY T24RAWOGG.T24_DDMEMO_ACTIVITY_PKG IS
     END CALC_ACCRUE_VAL_FUNC;
 
 ---------------------------------------------------------------------------
--- GEN_FROM_ACC_ECB_ARR_PROC
+-- GEN_FROM_ACC_ARR_ECB_LMT_PROC
 ---------------------------------------------------------------------------
-    PROCEDURE GEN_FROM_ACC_ECB_ARR_PROC IS 
+    PROCEDURE GEN_FROM_ACC_ARR_ECB_LMT_PROC IS 
         V_JOIN_KEY_LIST  T_JOIN_KEY_ARRAY;
         V_WINDOW_ID_LIST T_WINDOW_ID_ARRAY;
         V_TODAY          VARCHAR2(8);
     BEGIN
         SELECT CDC.JOIN_KEY, CDC.WINDOW_ID
         BULK COLLECT INTO V_JOIN_KEY_LIST, V_WINDOW_ID_LIST
-        FROM T24_DDMEMO_ACTIVITY_ACC_ARR_ECB CDC
+        FROM T24_DDMEMO_ACTIVITY_ACC_ARR_ECB_LMT CDC
         WHERE EXISTS (
             SELECT 1
             FROM V_FMSB_ECB_MAPPED ECB
@@ -184,12 +184,17 @@ CREATE OR REPLACE PACKAGE BODY T24RAWOGG.T24_DDMEMO_ACTIVITY_PKG IS
             SELECT 1
             FROM V_FMSB_ARR_MAPPED ARR
             WHERE ARR.WINDOW_ID = CDC.WINDOW_ID
+        )
+        OR EXISTS (
+            SELECT 1
+            FROM V_FMSB_LMT_MAPPED LMT
+            WHERE LMT.WINDOW_ID = CDC.WINDOW_ID
         );
         -- ) FETCH FIRST 9999 ROWS ONLY;
         
         IF V_JOIN_KEY_LIST.COUNT > 0 THEN
 
-            DELETE FROM T24_DDMEMO_ACTIVITY_ACC_ARR_ECB CDC
+            DELETE FROM T24_DDMEMO_ACTIVITY_ACC_ARR_ECB_LMT CDC
             WHERE EXISTS (
                 SELECT 1
                 FROM TABLE(V_WINDOW_ID_LIST) TMP
@@ -280,7 +285,7 @@ CREATE OR REPLACE PACKAGE BODY T24RAWOGG.T24_DDMEMO_ACTIVITY_PKG IS
                 PRE.COMMIT_TS,
                 PRE.REPLICAT_TS,
                 PRE.MAPPED_TS,
-                'ACC_ECB_ARR'
+                'ACC_ARR_ECB_LMT'
             FROM PRECOMPUTED PRE
             LEFT JOIN AGGREGATED AGG ON AGG.ARRANGEMENT = PRE.ARR_RECID;
 
@@ -291,127 +296,7 @@ CREATE OR REPLACE PACKAGE BODY T24RAWOGG.T24_DDMEMO_ACTIVITY_PKG IS
         WHEN OTHERS THEN
             ROLLBACK;
             RAISE;
-    END GEN_FROM_ACC_ECB_ARR_PROC;
-
----------------------------------------------------------------------------
--- GEN_FROM_LMT_PROC
----------------------------------------------------------------------------
-    PROCEDURE GEN_FROM_LMT_PROC IS
-        V_WINDOW_ID_LIST T_WINDOW_ID_ARRAY;
-        V_TODAY VARCHAR2(8);
-    BEGIN
-        SELECT CDC.WINDOW_ID
-        BULK COLLECT INTO V_WINDOW_ID_LIST
-        FROM T24_DDMEMO_ACTIVITY_LMT CDC
-        WHERE EXISTS (
-            SELECT 1
-            FROM V_FMSB_LMT_MAPPED LMT
-            WHERE LMT.WINDOW_ID = CDC.WINDOW_ID
-        );
-        -- ) FETCH FIRST 9999 ROWS ONLY;
-
-        IF V_WINDOW_ID_LIST.COUNT > 0 THEN
-            -- SYS.DBMS_SESSION.SLEEP(0.5);
-
-            DELETE FROM T24_DDMEMO_ACTIVITY_LMT CDC
-            WHERE EXISTS (
-                SELECT 1
-                FROM TABLE(V_WINDOW_ID_LIST) TMP
-                WHERE TMP.COLUMN_VALUE = CDC.WINDOW_ID
-            );
-
-            SELECT /*+ RESULT_CACHE */ TODAY INTO V_TODAY
-            FROM F_DAT_MAPPED
-            WHERE RECID = 'VN0011000';
-
-            INSERT INTO T24_DDMEMO_ACTIVITY (
-                BRANCH, ACCTNO, ACNAME, CIFNO, STATUS, SCCODE,
-                DLA7, DLA6, HOLD, CBAL, ACCRUE, ODLIMT, 
-                WINDOW_ID, COMMIT_TS, REPLICAT_TS, MAPPED_TS, CALL_CDC
-            )
-            WITH PRECOMPUTED AS(
-                SELECT /*+ MATERIALIZE */
-                    ACC.CO_CODE          AS BRANCH,
-                    ACC.RECID            AS ACCTNO,
-                    ARR.RECID            AS ARR_RECID,
-                    ACC.ACNAME           AS ACNAME,
-                    ACC.CUSTOMER         AS CIFNO,
-                    ARR.ARR_STATUS       AS ARR_STATUS,
-                    PST.RESTRICTION_TYPE AS RESTRICTION_TYPE,
-                    ADL.DORMANCY_STATUS  AS DORMANCY_STATUS,
-                    ARR.START_DATE       AS START_DATE,
-                    ARR.ACTIVE_PRODUCT   AS SCCODE,
-                    ACC.LOCKED_AMOUNT    AS HOLD,
-                    ECB.CURR_ASSET_TYPE  AS CURR_ASSET_TYPE,
-                    ECB.OPEN_BALANCE     AS OPEN_BALANCE,
-                    ECB.CREDIT_MVMT      AS CREDIT_MVMT,
-                    ECB.DEBIT_MVMT       AS DEBIT_MVMT,
-                    LMT.INTERNAL_AMOUNT  AS ODLIMT,
-                    LMT.WINDOW_ID        AS WINDOW_ID,
-                    LMT.COMMIT_TS        AS COMMIT_TS,
-                    LMT.REPLICAT_TS      AS REPLICAT_TS,
-                    LMT.MAPPED_TS        AS MAPPED_TS
-                FROM TABLE(V_WINDOW_ID_LIST) TMP
-                INNER JOIN V_FMSB_LMT_MAPPED LMT ON LMT.WINDOW_ID      = TMP.COLUMN_VALUE
-                INNER JOIN V_FMSB_ACC_MAPPED ACC ON ACC.LIMIT_KEY      = LMT.RECID
-                INNER JOIN V_FMSB_ARR_MAPPED ARR ON ARR.LINKED_APPL_ID = ACC.RECID
-                INNER JOIN V_FMSB_ECB_MAPPED ECB ON ECB.RECID          = ACC.RECID
-                LEFT JOIN  V_FMSB_ADL_MAPPED ADL ON ADL.RECID          = ARR.RECID
-                LEFT JOIN  F_PST_MAPPED      PST ON PST.RECID          = T24_UTILS_PKG.GET_STR_VAL_BY_POS_FUNC(ACC.POSTING_RESTRICT, '#1:')   
-                WHERE ARR.PRODUCT_LINE = 'ACCOUNTS' AND ARR.ARR_STATUS <>'UNAUTH'             
-            ),
-            AGGREGATED AS (
-                SELECT 
-                    ARRANGEMENT,
-                    MAX(EFFECTIVE_DATE) AS MAX_EFF_DAT
-                FROM V_FMSB_ARC_DDMEMO ARC
-                WHERE EXISTS (
-                    SELECT 1 FROM PRECOMPUTED PRE
-                    WHERE PRE.ARR_RECID = ARC.ARRANGEMENT)
-                GROUP BY ARRANGEMENT
-            )   
-            SELECT
-                PRE.BRANCH,
-                TO_NUMBER(PRE.ACCTNO),
-                TRIM(PRE.ACNAME),
-                TO_NUMBER(PRE.CIFNO),
-                CASE
-                    WHEN PRE.ARR_STATUS IN ('CLOSE', 'PENDING.CLOSURE', 'CANCELLED') THEN 2
-                    WHEN PRE.RESTRICTION_TYPE = 'DEBIT' THEN 6
-                    WHEN PRE.RESTRICTION_TYPE = 'ALL' THEN 7
-                    WHEN PRE.DORMANCY_STATUS IS NOT NULL THEN 9
-                    WHEN PRE.START_DATE = V_TODAY AND PRE.ARR_STATUS NOT IN ('CLOSE', 'PENDING.CLOSURE') THEN 4
-                    WHEN PRE.ARR_STATUS IN ('AUTH', 'RESTORE-AUTH') AND PRE.DORMANCY_STATUS IS NULL THEN 1
-                END,
-                NVL(PRE.SCCODE, ' ') AS SCCODE,
-                NVL(
-                    TO_NUMBER(TO_CHAR(AGG.MAX_EFF_DAT, 'YYYYDDD')),
-                    TO_NUMBER(TO_CHAR(TO_DATE(V_TODAY, 'YYYYMMDD'), 'YYYYDDD'))
-                ) AS DLA7,
-                NVL(
-                    TO_NUMBER(TO_CHAR(AGG.MAX_EFF_DAT, 'DDMMYY')),
-                    TO_NUMBER(TO_CHAR(TO_DATE(V_TODAY, 'YYYYMMDD'), 'DDMMYY'))
-                ) AS DLA6,
-                CALC_HOLD_VAL_FUNC(PRE.HOLD) AS HOLD,
-                CALC_CBAL_VAL_FUNC(PRE.CURR_ASSET_TYPE, PRE.OPEN_BALANCE, PRE.CREDIT_MVMT, PRE.DEBIT_MVMT),
-                CALC_ACCRUE_VAL_FUNC(PRE.CURR_ASSET_TYPE, PRE.OPEN_BALANCE, PRE.CREDIT_MVMT, PRE.DEBIT_MVMT),
-                TO_NUMBER(NVL(PRE.ODLIMT,0)),
-                PRE.WINDOW_ID,
-                PRE.COMMIT_TS,
-                PRE.REPLICAT_TS,
-                PRE.MAPPED_TS,
-                'LMT'
-            FROM PRECOMPUTED PRE
-            LEFT JOIN AGGREGATED AGG ON AGG.ARRANGEMENT = PRE.ARR_RECID;
-
-            COMMIT;
-        END IF;
-
-    EXCEPTION
-        WHEN OTHERS THEN
-            ROLLBACK;
-            RAISE;
-    END GEN_FROM_LMT_PROC;
+    END GEN_FROM_ACC_ARR_ECB_LMT_PROC;
  
 ---------------------------------------------------------------------------
 -- GEN_FROM_ADL_PROC
